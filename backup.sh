5334081713dab9069f57ac75653ffc4d835747bb
Hussein-Mourad <38833034+Hussein-Mourad@users.noreply.github.com>
2022-01-01 02:23:32 +0200

    backup

--- backup.sh
+++ backup.sh
@@ -9,6 +9,6 @@ ls $HOME/.local/share/gnome-shell/extensions > $DEST/.local/share/gnome-shell/ex
 rsync -a --info=progress2 -h /etc/apt/sources.list  $DEST/etc/sources.list
 rsync -a --info=progress2 -h /etc/apt/sources.list.d $DEST/etc/sources.list.d
 rsync -a --info=progress2 -h /etc/fstab $DEST/etc/fstab
-rsync -a --info=progress2 -h $HOME/.zshrc $HOME/.themes $HOME/.icons $HOME/.fonts $HOME/.bashrc $HOME/.vimrc j $HOME/.SpaceVim.d $HOME/.vim_back $HOME/.gitconfig $DEST/home-folder
+rsync -a --info=progress2 -h $HOME/.zshrc $HOME/.themes $HOME/.icons $HOME/.fonts $HOME/.bashrc $HOME/.vimrc $HOME/.SpaceVim.d $HOME/.vim_back $HOME/.gitconfig $DEST/home-folder
 rsync -a --info=progress2 -h $HOME/.config/i3 $HOME/.config/autostart $DEST/.config
 

eda05e3acb50042b158f2642cc4ee9a1efe314bd
Hussein-Mourad <38833034+Hussein-Mourad@users.noreply.github.com>
2022-03-21 11:07:12 +0200

    added nala script

---
+++ installers/nala-zsh.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/zsh
+
+autoload bashcompinit;
+bashcompinit;
+echo 'source /usr/share/bash-completion/completions/nala' >> ~/.zshrc
+
---
+++ installers/nala.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+echo "deb [arch=amd64,arm64,armhf] http://deb.volian.org/volian/ scar main" | sudo tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list;
+wget -qO - https://deb.volian.org/volian/scar.key | sudo tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg > /dev/null;
+sudo apt update && sudo apt install nala;
+

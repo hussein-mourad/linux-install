6602f20059a15e103ebe392a46507265c9c82f2f
Hussein-Mourad <38833034+Hussein-Mourad@users.noreply.github.com>
2022-09-16 01:35:02 +0200

    backup

---
+++ installers/ros-packages.sh
@@ -0,0 +1,4 @@
+sudo apt -y install ros-noetic-rosserial-arduino ros-noetic-rosserial;
+sudo apt -y install ros-noetic-rqt-ez-publisher;
+
+
---
+++ installers/ros2-install.sh
@@ -0,0 +1,24 @@
+locale  # check for UTF-8
+
+sudo apt update && sudo apt install locales
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+locale  # verify settings
+
+apt-cache policy | grep universe
+
+sudo apt install software-properties-common
+sudo add-apt-repository universe
+
+sudo apt update && sudo apt install curl gnupg2 lsb-release
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /usr/share/keyrings/ros-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+sudo apt update && sudo apt upgrade -y;
+
+sudo apt install ros-foxy-desktop;
+sudo apt install python3-colcon-common-extensions
+

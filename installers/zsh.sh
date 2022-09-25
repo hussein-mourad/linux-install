bb78a6574e2b5f2ccb41a54be514445cde8a4deb
Hussein-Mourad <38833034+Hussein-Mourad@users.noreply.github.com>
2022-09-25 04:23:42 +0200

    update

--- installers/zsh.sh
+++ installers/zsh.sh
@@ -12,8 +12,9 @@ sudo apt -y install zsh;
 curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh > ohmyzsh-install.sh;
 
 # Antigen
-sudo mkdir -p /usr/share/antigen/;
-curl -L git.io/antigen > antigen.zsh;
-sudo mv antigen.zsh /usr/share/antigen/;
-sudo chmod +x /usr/share/antigen/antigen.zsh;
+sudo apt install zsh-antigen;
+#sudo mkdir -p /usr/share/antigen/;
+#curl -L git.io/antigen > antigen.zsh;
+#sudo mv antigen.zsh /usr/share/antigen/;
+#sudo chmod +x /usr/share/antigen/antigen.zsh;
 
